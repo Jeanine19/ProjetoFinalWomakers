8ab38f0ae32656d8481d7c6b0bc1814a706d3081
Jeanine <jeanine.a.santiago@gmail.com>
2023-11-10 14:46:34 -0300

    inicio do JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const carousel = document.querySelector('.carousel');
+    const slides = document.querySelectorAll('.slide');
+    let currentIndex = 0;
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            slide.style.display = i === index ? 'block' : 'none';
+        });
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        showSlide(currentIndex);
+    }
+
+    function prevSlide() {
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        showSlide(currentIndex);
+    }
+
+    showSlide(currentIndex);
+
+    document.querySelector('.carousel').addEventListener('click', nextSlide);
+});
+
